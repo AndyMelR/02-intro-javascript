e2d910ec057e6afd8efaecae009490986ff476ae
Andrea Melero <ameleror@gmail.com>
2023-11-06 19:48:00 +0100

    filter method

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-const personajes = ['Goku', 'Vegeta', 'Trunks'];
 
-//console.log (personajes[0]);
-//console.log (personajes[1]);
-//console.log (personajes[2]);
+import { heroes } from './data/heroes'
+
+const getHeroeById = (id) => {
+    return (heroes.find((heroe)=> heroe.id === id)); 
+}
+
+console.log( getHeroeById(2))
+
+const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner);
+console.log(getHeroesByOwner('DC'));
